699552d5ae55948097653d4e034369ba8b697164
Solomon-Henry <cherm005@gmail.com>
2023-11-09 23:57:56 -0500

    RPS commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Header() {
+  return (
+    <div>
+      <p> <b>Rules</b> </p>
+      <pre>
+        Rock beats Scissors
+      </pre>
+      <pre>  
+        Paper beats Rock
+      </pre>
+      <pre>
+        Scissors beats Paper
+      </pre> 
+      <hr></hr>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Options(props) {
+  const [symbol, setSymbol] = useState(props.symbol);
+  return <button className='btn' onClick={props.onSquareClick}>{props.symbol}</button>
+}
+function Game() {
+
+  function renderOptions() {
+
+  }
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+/*
+
+Header
+- Score 
+
+Game
+- Options
+
+- Conditional: Result & Play Again
+
+*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
